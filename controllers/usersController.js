519c763c5f58dc7dbbe3a8061f3dee16b1856ef3
paruj_lab <paruj.lab@gmail.com>
2022-11-18 14:17:40 +0700

    feat:setAvatar

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -26,3 +26,59 @@ module.exports.register = async (req, res, next) => {
     next(err);
   }
 };
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.json({ msg: "Incorrect Username or Password", status: false });
+      const userParse = JSON.parse(JSON.stringify(user))
+    delete userParse.password;
+    return res.json({ status: true, user:userParse });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(userId, {
+      isAvatarImageSet: true,
+      avatarImage,
+    },
+    { new: true });
+    console.log(userData)
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(userId, {
+      isAvatarImageSet: true,
+      avatarImage,
+    },
+    { new: true });
+    console.log(userData)
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (ex) {
+    next(ex);
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const { register } = require("../controllers/usersController");
+const { register,login ,setAvatar } = require("../controllers/usersController");
+
 
 router.post("/register", register);
+router.post("/login", login);
+router.post("/setAvatar/:id", setAvatar);
 
 module.exports = router
