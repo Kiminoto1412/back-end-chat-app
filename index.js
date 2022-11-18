a6cbfcdd1598f110ceaad2d7718727615a9ceb90
paruj_lab <paruj.lab@gmail.com>
2022-11-18 17:01:14 +0700

    feat:add message schema ,add addmessageController

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose")
 
 const userRoutes = require("./routes/userRoutes")
+const messageRoutes = require("./routes/messageRoutes")
 
 const app = express();
 require("dotenv").config();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth",userRoutes)
+app.use("/message",messageRoutes)
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewURLParser:true,
